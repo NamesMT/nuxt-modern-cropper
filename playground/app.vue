312e12265af2a443e0dd3b191ffee93c629b8810
NamesMT <dangquoctrung123@gmail.com>
2024-08-06 15:27:25 +0000

    chore: adjust playground sample

--- playground/app.vue
+++ playground/app.vue
@@ -1,14 +1,33 @@
+<script setup lang="ts">
+import { ModernCropper } from '#components'
+
+const cropper = ref<InstanceType<typeof ModernCropper>>()
+
+onMounted(async () => {
+  await nextTick()
+
+  const cropperRef = cropper.value!
+
+  console.log({ a: cropperRef, b: cropperRef.selection })
+
+  cropperRef.onCropperMounted(({ selection }) => {
+    console.log({ a: cropperRef, b: selection })
+    console.log({ a: cropperRef, b: cropperRef.selection })
+  })
+})
+</script>
+
 <template>
   <div>
     Nuxt module playground!
-    <ModernCropper
-      :pass-through="{
-        canvas: { attributes: { background: false } },
-      }"
-      src="https://i.imgur.com/DkRVUyG.png"
-    />
+    <ClientOnly>
+      <ModernCropper
+        ref="cropper"
+        :pass-through="{
+          canvas: { attributes: { background: false } },
+        }"
+        src="https://i.imgur.com/DkRVUyG.png"
+      />
+    </ClientOnly>
   </div>
 </template>
-
-<script setup>
-</script>
