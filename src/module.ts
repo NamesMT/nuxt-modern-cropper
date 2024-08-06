03fd968e01fa49f6d31070c5cb4884805503f58a
NamesMT <dangquoctrung123@gmail.com>
2024-08-06 15:28:23 +0000

    style: apply lint fixes

--- src/module.ts
+++ src/module.ts
@@ -1,6 +1,7 @@
 import { defineNuxtModule, addComponent } from '@nuxt/kit'
 
 // Module options TypeScript interface definition
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export interface ModuleOptions {}
 
 export default defineNuxtModule<ModuleOptions>({
